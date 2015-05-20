af0397d0e910cb713ac4262923de1530768abb09
Simone Basso <bassosimone@gmail.com>
2015-05-20 15:06:34 +0200

    Rewrite of config.py now complete

--- neubot/config.py
+++ neubot/config.py
@@ -1,9 +1,7 @@
-# neubot/config.py
-
 #
-# Copyright (c) 2010-2011, 2013
-#     Nexa Center for Internet & Society, Politecnico di Torino (DAUIN)
-#     and Simone Basso <bassosimone@gmail.com>
+# Copyright (c) 2015
+#   Nexa Center for Internet & Society, Politecnico di Torino (Italy)
+#   and Simone Basso <bassosimone@gmail.com>.
 #
 # This file is part of Neubot <http://www.neubot.org/>.
 #
@@ -21,46 +19,23 @@
 # along with Neubot.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-import logging
+""" Configurable variables """
 
-from neubot import utils
-
-class Config(object):
-
-    """Configuration manager"""
-
-    def __init__(self):
-        self.conf = {}
-
-    def copy(self):
-        return dict(self.conf)
-
-    def get(self, key, defvalue):
-        return self.conf.get(key, defvalue)
-
-    def __getitem__(self, key):
-        return self.conf[key]
-
-    def __setitem__(self, key, value):
-        self.conf[key] = value
-
-CONFIG = Config()
-
-CONFIG.update({
-    'bittorrent.address': '',
-    'bittorrent.bytes.down': 0,
-    'bittorrent.bytes.up': 0,
-    'bittorrent.infohash': '',
-    'bittorrent.listen': False,
-    'bittorrent.negotiate': True,
-    'bittorrent.negotiate.port': 8080,
-    'bittorrent.my_id': '',
-    'bittorrent.numpieces': 1 << 20,
-    'bittorrent.piece_len': 1 << 17,
-    'bittorrent.port': 6881,
-    'bittorrent.watchdog': 300,
-    'negotiate.parallelism': 7,
-    'negotiate.min_thresh': 32,
-    'negotiate.max_thresh': 64,
+CONFIG = {
+    "bittorrent.address": "",
+    "bittorrent.bytes.down": 0,
+    "bittorrent.bytes.up": 0,
+    "bittorrent.infohash": "",
+    "bittorrent.listen": False,
+    "bittorrent.negotiate": True,
+    "bittorrent.negotiate.port": 8080,
+    "bittorrent.my_id": "",
+    "bittorrent.numpieces": 1 << 20,
+    "bittorrent.piece_len": 1 << 17,
+    "bittorrent.port": 6881,
+    "bittorrent.watchdog": 300,
+    "negotiate.parallelism": 7,
+    "negotiate.min_thresh": 32,
+    "negotiate.max_thresh": 64,
     "prefer_ipv6": 0,
-})
+}
