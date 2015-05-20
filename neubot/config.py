dc31b2f53740d4ef2a05f479a738828a96d8b35e
Simone Basso <bassosimone@gmail.com>
2015-05-20 11:59:34 +0200

    config.py: get rid of ConfigDict
    This was only used to ensure that wrong types were not set
    by the web user interface, which here does not exist.

--- neubot/config.py
+++ neubot/config.py
@@ -20,56 +20,17 @@
 # You should have received a copy of the GNU General Public License
 # along with Neubot.  If not, see <http://www.gnu.org/licenses/>.
 #
-# =================================================================
-# The update() method of ConfigDict is a derivative work
-# of Python 2.5.2 Object/dictobject.c dict_update_common().
-#
-# Copyright (c) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008
-# Python Software Foundation; All Rights Reserved.
-#
-# I've put a copy of Python LICENSE file at doc/LICENSE.Python.
-# =================================================================
-#
 
 import logging
 
 from neubot import utils
-
-class ConfigDict(dict):
-
-    """Modified dictionary.  At the beginning we fill it with default
-       values, e.g. when a new module is loaded.  Then, when we update
-       this dictionary we want new values to have the same type of the
-       old ones.  We perform the check when the dictionary is updated
-       and not when it is accessed because in the latter case we might
-       delay errors and that would be surprising."""
-
-    def __setitem__(self, key, value):
-        if key in self:
-            ovalue = self[key]
-            cast = utils.smart_cast(ovalue)
-        else:
-            ovalue = "(none)"
-            cast = utils.smart_cast(value)
-        value = cast(value)
-        logging.debug("config: %s: %s -> %s", key, ovalue, value)
-        dict.__setitem__(self, key, value)
-
-    def update(self, *args, **kwds):
-        if args:
-            arg = tuple(args)[0]
-            if hasattr(arg, "keys"):
-                arg = arg.iteritems()
-            map(lambda t: self.__setitem__(t[0], t[1]), arg)
-        if kwds:
-            self.update(kwds.iteritems())
 
 class Config(object):
 
     """Configuration manager"""
 
     def __init__(self):
-        self.conf = ConfigDict()
+        self.conf = {}
         self.descriptions = {}
 
     def register_defaults(self, kvstore):
