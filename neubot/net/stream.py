e7f85a3cfc804cce906834df0617faf9d9611ca5
Simone Basso <bassosimone@gmail.com>
2015-05-23 13:55:03 +0200

    stream.py: use AsyncSocket, remove SSL

--- neubot/net/stream.py
+++ neubot/net/stream.py
@@ -27,103 +27,22 @@
 # along with Neubot.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-# Will be replaced by neubot/stream.py 
-
 import collections
-import errno
-import socket
 import types
 import logging
 
-try:
-    import ssl
-except ImportError:
-    ssl = None
-
-from neubot.config import CONFIG
 from neubot.log import oops
 from .pollable import Pollable
+from .pollable import SUCCESS
+from .pollable import WANT_READ
+from .pollable import WANT_WRITE
+from .pollable import CONNRST
+from .async_socket import AsyncSocket
 
-from neubot import utils
 from neubot.utils import utils_net
-
-# States returned by the socket model
-STATES = [SUCCESS, ERROR, WANT_READ, WANT_WRITE, CONNRESET] = range(5)
 
 # Maximum amount of bytes we read from a socket
 MAXBUF = 1 << 18
-
-# Soft errors on sockets, i.e. we can retry later
-SOFT_ERRORS = [ errno.EAGAIN, errno.EWOULDBLOCK, errno.EINTR ]
-
-if ssl:
-    class SSLWrapper(object):
-        def __init__(self, sock):
-            self.sock = sock
-
-        def soclose(self):
-            try:
-                self.sock.close()
-            except ssl.SSLError:
-                logging.error('Exception', exc_info=1)
-
-        def sorecv(self, maxlen):
-            try:
-                octets = self.sock.read(maxlen)
-                return SUCCESS, octets
-            except ssl.SSLError, exception:
-                if exception[0] == ssl.SSL_ERROR_WANT_READ:
-                    return WANT_READ, ""
-                elif exception[0] == ssl.SSL_ERROR_WANT_WRITE:
-                    return WANT_WRITE, ""
-                else:
-                    return ERROR, exception
-
-        def sosend(self, octets):
-            try:
-                count = self.sock.write(octets)
-                return SUCCESS, count
-            except ssl.SSLError, exception:
-                if exception[0] == ssl.SSL_ERROR_WANT_READ:
-                    return WANT_READ, 0
-                elif exception[0] == ssl.SSL_ERROR_WANT_WRITE:
-                    return WANT_WRITE, 0
-                else:
-                    return ERROR, exception
-
-class SocketWrapper(object):
-    def __init__(self, sock):
-        self.sock = sock
-
-    def soclose(self):
-        try:
-            self.sock.close()
-        except socket.error:
-            logging.error('Exception', exc_info=1)
-
-    def sorecv(self, maxlen):
-        try:
-            octets = self.sock.recv(maxlen)
-            return SUCCESS, octets
-        except socket.error, exception:
-            if exception[0] in SOFT_ERRORS:
-                return WANT_READ, ""
-            elif exception[0] == errno.ECONNRESET:
-                return CONNRESET, ""
-            else:
-                return ERROR, exception
-
-    def sosend(self, octets):
-        try:
-            count = self.sock.send(octets)
-            return SUCCESS, count
-        except socket.error, exception:
-            if exception[0] in SOFT_ERRORS:
-                return WANT_WRITE, 0
-            elif exception[0] == errno.ECONNRESET:
-                return CONNRESET, 0
-            else:
-                return ERROR, exception
 
 class Stream(Pollable):
     def __init__(self, poller):
@@ -142,10 +61,7 @@ class Stream(Pollable):
 
         self.close_complete = False
         self.close_pending = False
-        self.recv_blocked = False
         self.recv_pending = False
-        self.recv_ssl_needs_kickoff = False
-        self.send_blocked = False
         self.send_octets = None
         self.send_queue = collections.deque()
         self.send_pending = False
@@ -174,25 +90,7 @@ class Stream(Pollable):
 
         logging.debug("* Connection made %s", str(self.logname))
 
-        if conf.get("net.stream.secure", 0):
-            if not ssl:
-                raise RuntimeError("SSL support not available")
-
-            server_side = conf["net.stream.server_side"]
-            certfile = conf["net.stream.certfile"]
-
-            # wrap_socket distinguishes between None and ''
-            if not certfile:
-                certfile = None
-
-            ssl_sock = ssl.wrap_socket(sock, do_handshake_on_connect=False,
-              certfile=certfile, server_side=server_side)
-            self.sock = SSLWrapper(ssl_sock)
-
-            self.recv_ssl_needs_kickoff = not server_side
-
-        else:
-            self.sock = SocketWrapper(sock)
+        self.sock = AsyncSocket(sock)
 
         self.connection_made()
 
@@ -244,42 +142,13 @@ class Stream(Pollable):
 
     def start_recv(self):
         if (self.close_complete or self.close_pending
-          or self.recv_pending):
+                or self.recv_pending):
             return
 
         self.recv_pending = True
-
-        if self.recv_blocked:
-            return
-
         self.poller.set_readable(self)
 
-        #
-        # The client-side of an SSL connection must send the HELLO
-        # message to start the negotiation.  This is done automagically
-        # by SSL_read and SSL_write.  When the client first operation
-        # is a send(), no problem: the socket is always writable at
-        # the beginning and writable() invokes sosend() that invokes
-        # SSL_write() that negotiates.  The problem is when the client
-        # first operation is recv(): in this case the socket would never
-        # become readable because the server side is waiting for an HELLO.
-        # The following piece of code makes sure that the first recv()
-        # of the client invokes readable() that invokes sorecv() that
-        # invokes SSL_read() that starts the negotiation.
-        #
-        if self.recv_ssl_needs_kickoff:
-            self.recv_ssl_needs_kickoff = False
-            self.handle_read()
-
     def handle_read(self):
-        if self.recv_blocked:
-            self.poller.set_writable(self)
-            if not self.recv_pending:
-                self.poller.unset_readable(self)
-            self.recv_blocked = False
-            self.handle_write()
-            return
-
         status, octets = self.sock.sorecv(MAXBUF)
 
         if status == SUCCESS and octets:
@@ -294,13 +163,7 @@ class Stream(Pollable):
         if status == WANT_READ:
             return
 
-        if status == WANT_WRITE:
-            self.poller.unset_readable(self)
-            self.poller.set_writable(self)
-            self.send_blocked = True
-            return
-
-        if status == CONNRESET and not octets:
+        if status == CONNRST and not octets:
             self.rst = True
             self.poller.close(self)
             return
@@ -309,10 +172,6 @@ class Stream(Pollable):
             self.eof = True
             self.poller.close(self)
             return
-
-        if status == ERROR:
-            # Here octets is the exception that occurred
-            raise octets
 
         raise RuntimeError("Unexpected status value")
 
@@ -358,21 +217,9 @@ class Stream(Pollable):
             return
 
         self.send_pending = True
-
-        if self.send_blocked:
-            return
-
         self.poller.set_writable(self)
 
     def handle_write(self):
-        if self.send_blocked:
-            self.poller.set_readable(self)
-            if not self.send_pending:
-                self.poller.unset_writable(self)
-            self.send_blocked = False
-            self.handle_read()
-            return
-
         status, count = self.sock.sosend(self.send_octets)
 
         if status == SUCCESS and count > 0:
@@ -402,17 +249,7 @@ class Stream(Pollable):
         if status == WANT_WRITE:
             return
 
-        if status == WANT_READ:
-            self.poller.unset_writable(self)
-            self.poller.set_readable(self)
-            self.recv_blocked = True
-            return
-
-        if status == ERROR:
-            # Here count is the exception that occurred
-            raise count
-
-        if status == CONNRESET and count == 0:
+        if status == CONNRST and count == 0:
             self.rst = True
             self.poller.close(self)
             return
